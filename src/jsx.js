9ad610c4d1dd745742817cb70df1dcb95fd7b4dd
Nicolas Gonzalvez <cristopher.gonzalvez@gmail.com>
2016-11-05 03:17:27 -0300

    Change library name to cgx-jsx.

--- src/jsx.js
+++ src/jsx.js
@@ -1,10 +1,13 @@
-function __e(tag, attributes) {
+function $h(tag, attributes) {
   var d = document;
   var args = arguments;
   var i;
 
+  // Standard tags.
   if (typeof tag === 'string') {
     tag = d.createElement(tag);
+
+  // Components with a render method.
   } else if (tag.prototype.render) {
     tag = (new tag()).render();
   }
@@ -13,6 +16,7 @@ function __e(tag, attributes) {
     tag.setAttribute(i, attributes[i]);
   }
 
+  // Append all given children.
   for (i = 2; i < args.length; i++) {
     tag.appendChild(typeof child === 'string' ? d.createTextNode(args[i])
                                               : args[i]);
